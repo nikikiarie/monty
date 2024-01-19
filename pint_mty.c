5dd76de02f2571ba8854980082a4cc62e86d4949
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-19 08:18:15 -0800

    clean

--- pint_mty.c
+++ pint_mty.c
@@ -9,7 +9,7 @@ void pint_mty(stack_t **head, unsigned int count)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr,"L%u: can't pint, stack empty\n", count);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
 		fclose(b.file);
 		free(b.line);
 		st_free(*head);
